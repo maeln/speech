f3ae261475610b6d50f8d5c5b589d5065dcc9d21
Maël Naccache Tüfekçi <contact@maeln.com>
2021-05-01 21:14:11 +0200

    Quelques sourires - Matmatah

--- speech.cpp
+++ speech.cpp
@@ -3,19 +3,70 @@
 #define WINDOWS_IGNORE_PACKING_MISMATCH
 #include <windows.h>
 #include <sapi.h>
-#include "mmsystem.h"
-#include "mmreg.h"
+#include <mmsystem.h>
+#include <mmreg.h>
 
-// cl speech.cpp /nologo /O1 /Ob1 /Oi /Os /Oy /GF /GS- /Zp1 /Gz /FAs /Fp /c /utf-8
-// link speech.obj /DYNAMICBASE:NO  /MANIFEST:NO /INCREMENTAL:NO  /NODEFAULTLIB /TLBID:1 /MACHINE:X86 /SUBSYSTEM:WINDOWS /ENTRY:"entrypoint" "kernel32.lib" "ole32.lib" "winmm.lib"
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    int _fltused = 0; // it should be a single underscore since the double one is the mangled name
+#ifdef __cplusplus
+}
+#endif
+
+// gl
+#include <gl/GL.h>
+#include "glext.h"
+#include "gldefs.h"
 
 const wchar_t truc[] =
-    L"édjé je t'aime!";
+    L"bonjour monde";
+
+const wchar_t window_class[] = L"static";
+
+static const PIXELFORMATDESCRIPTOR pfd = {
+    sizeof(PIXELFORMATDESCRIPTOR), 1, PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER, PFD_TYPE_RGBA,
+    32, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 32, 0, 0, PFD_MAIN_PLANE, 0, 0, 0, 0};
+
+static DEVMODE screenSettings = {{0},
+                                 0,
+                                 0,
+                                 156,
+                                 0,
+                                 0x001c0000,
+                                 {0},
+                                 0,
+                                 0,
+                                 0,
+                                 0,
+                                 0,
+                                 {0},
+                                 0,
+                                 32,
+                                 1920,
+                                 1280,
+                                 {0},
+                                 0,
+#if (WINVER >= 0x0400)
+                                 0,
+                                 0,
+                                 0,
+                                 0,
+                                 0,
+                                 0,
+#if (WINVER >= 0x0500) || (_WIN32_WINNT >= 0x0400)
+                                 0,
+                                 0
+#endif
+#endif
+};
 
 void entrypoint(void)
 {
     CoInitialize(NULL);
 
+    // init voice
     ISpVoice *pVoice = NULL;
     CLSID _CLSID_SpVoice;
     CLSIDFromString(L"{96749377-3391-11D2-9EE3-00C04F797396}", &_CLSID_SpVoice);
@@ -23,6 +74,7 @@ void entrypoint(void)
     CLSIDFromString(L"{6C44DF74-72B9-4992-A1EC-EF996E0422D4}", &_IID_ISpVoice);
     HRESULT hr = CoCreateInstance(_CLSID_SpVoice, NULL, CLSCTX_ALL, _IID_ISpVoice, (void **)&pVoice);
 
+    // init stream to output to
     IStream *pStream = NULL;
     CreateStreamOnHGlobal(NULL, TRUE, &pStream);
     CLSID _CLSID_SpStream;
@@ -31,6 +83,8 @@ void entrypoint(void)
     CLSIDFromString(L"{12E3CCA9-7518-44C5-A5E7-BA5A79CB929E}", &_IID_ISpStream);
     ISpStream *pSpStream = nullptr;
     hr = CoCreateInstance(_CLSID_SpStream, NULL, CLSCTX_ALL, _IID_ISpStream, (void **)&pSpStream);
+
+    // WAV format to use
     WAVEFORMATEX fmt = {};
     fmt.wFormatTag = WAVE_FORMAT_PCM;
     fmt.nChannels = 2;
@@ -43,10 +97,12 @@ void entrypoint(void)
     GUID _SPDFID_WaveFormatEx;
     IIDFromString(L"{C31ADBAE-527F-4FF5-A230-F62BB61FF70C}", (IID *)&_SPDFID_WaveFormatEx);
 
+    // Speak in the stream
     hr = pSpStream->SetBaseStream(pStream, _SPDFID_WaveFormatEx, &fmt);
     hr = pVoice->SetOutput(pSpStream, TRUE);
     pVoice->Speak(truc, SPF_IS_XML, NULL);
 
+    // Dump stream in a buffer
     STATSTG stat;
     pStream->Stat(&stat, 0);
     unsigned char *data = (unsigned char *)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, stat.cbSize.LowPart);
@@ -55,6 +111,7 @@ void entrypoint(void)
     unsigned long c = 0;
     pStream->Read(data, stat.cbSize.LowPart, &c);
 
+    // Start to play the buffer
     HWAVEOUT hWaveOut;
     WAVEHDR WaveHDR = {(LPSTR)data, c, 0, 0, 0, 0, 0, 0};
     MMTIME MMTime = {TIME_BYTES, 0};
@@ -62,20 +119,38 @@ void entrypoint(void)
     waveOutPrepareHeader(hWaveOut, &WaveHDR, sizeof(WaveHDR));
     waveOutWrite(hWaveOut, &WaveHDR, sizeof(WaveHDR));
 
+    // Window param
+    //if (ChangeDisplaySettings(&screenSettings, CDS_FULLSCREEN) != DISP_CHANGE_SUCCESSFUL)
+    //    return;
+    //ShowCursor(0);
+    // create window
+    HDC hDC = GetDC(CreateWindow((LPCSTR)0xC018, 0, WS_POPUP | WS_VISIBLE, 0, 0, 800, 600, 0, 0, 0, 0));
+
+    // initalize opengl
+    SetPixelFormat(hDC, ChoosePixelFormat(hDC, &pfd), &pfd);
+    wglMakeCurrent(hDC, wglCreateContext(hDC));
+    SwapBuffers(hDC);
+
+    // main loop : Play the WAV until finished.
     bool finished = false;
     long t;
     long to = timeGetTime();
     do
     {
+        glClearColor(0, 0, 0, 0);
+        glClear(GL_COLOR_BUFFER_BIT);
         t = timeGetTime() - to;
         waveOutGetPosition(hWaveOut, &MMTime, sizeof(MMTIME));
+        SwapBuffers(hDC);
         finished = !(MMTime.u.cb < c);
-    } while (!finished);
+    } while (!finished && !GetAsyncKeyState(VK_ESCAPE));
 
     pSpStream->Release();
     pStream->Release();
     pVoice->Release();
 
     CoUninitialize();
+    //ChangeDisplaySettings(0, 0);
+    //ShowCursor(1);
     ExitProcess(0);
 }
