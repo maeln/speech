32c10463c8829aa3b0d0078bf1634afcf32c9b49
Maël Naccache Tüfekçi <contact@maeln.com>
2021-05-17 13:07:28 +0200

    does not crash when losing focus.

--- speech.cpp
+++ speech.cpp
@@ -24,10 +24,20 @@ extern "C"
 #include "draw_shader.h"
 #include "postprocess_shader.h"
 
-LRESULT CALLBACK WindowProc(HWND hWnd,
-                            UINT message,
-                            WPARAM wParam,
-                            LPARAM lParam);
+static void processMessages()
+{
+    MSG msg;
+    while (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+    {
+        if (msg.message == WM_QUIT)
+        {
+            // Kill
+            ExitProcess(0);
+        }
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+}
 
 const wchar_t truc[] =
     L"<voice required=\"Language=409\">What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little clever comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.</voice>";
@@ -169,7 +179,7 @@ int __stdcall WinMainCRTStartup()
         glRecti(-1, -1, 1, 1);
 
         SwapBuffers(hDC);
-
+        processMessages();
         // finished = !(MMTime.u.cb < c);
     } while (!finished && !GetAsyncKeyState(VK_ESCAPE));
 
@@ -181,25 +191,4 @@ int __stdcall WinMainCRTStartup()
     ShowCursor(true);
     ExitProcess(0);
     return 0;
-}
-
-LRESULT CALLBACK WindowProc(HWND hWnd,
-                            UINT message,
-                            WPARAM wParam,
-                            LPARAM lParam)
-{
-    switch (message)
-    {
-    case WM_DESTROY:
-    {
-        //ShowWindow( GetConsoleWindow(), SW_RESTORE );
-        PostQuitMessage(0);
-        return 0;
-    }
-    case WM_KILLFOCUS:
-        return 0;
-    case WM_SETFOCUS:
-        return 0;
-    }
-    return DefWindowProc(hWnd, message, wParam, lParam);
 }
